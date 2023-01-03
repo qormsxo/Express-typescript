c0432fb92200b9926f3e34d8929308371d003121
qormsxo <45680973+qormsxo@users.noreply.github.com>
2023-01-04 08:21:40 +0900

    안쓰는 함수

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -6,30 +6,9 @@ import { RequestWithUser } from '@interfaces/auth.interface';
 class UsersController {
   public userService = new userService();
 
-  public getUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      const findAllUsersData: User[] = await this.userService.findAllUser();
-
-      res.status(200).json({ data: findAllUsersData, message: 'findAll' });
-    } catch (error) {
-      next(error);
-    }
-  };
-
   public profileView(req: Request, res: Response) {
     res.render('profile', { title: '내 정보 - NodeBird' });
   }
-
-  public getUserById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      const userId = Number(req.params.id);
-      const findOneUserData: User = await this.userService.findUserById(userId);
-
-      res.status(200).json({ data: findOneUserData, message: 'findOne' });
-    } catch (error) {
-      next(error);
-    }
-  };
 
   public addFollowing = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -63,17 +42,6 @@ class UsersController {
       next(error);
     }
   };
-
-  // public deleteUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  //   try {
-  //     const userId = Number(req.params.id);
-  //     const deleteUserData: User = await this.userService.deleteUser(userId);
-
-  //     res.status(200).json({ data: deleteUserData, message: 'deleted' });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // };
 }
 
 export default UsersController;
