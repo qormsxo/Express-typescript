76ae6efb3e55d36f2dccca27750fc646595d8caa
qormsxo <45680973+qormsxo@users.noreply.github.com>
2023-01-04 08:20:59 +0900

    안쓰는 함수 지우기

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -21,44 +21,6 @@ class AuthService extends Repository<Users> {
     const createUserData: User = await Users.create({ ...userData, password: hashedPassword }).save();
     return createUserData;
   }
-
-  // 로그인 패스포트로 변경 예정
-  public async login(userData: CreateUserDto): Promise<{ cookie: string; findUser: User }> {
-    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
-
-    const findUser: User = await Users.findOne({ where: { email: userData.email } });
-    if (!findUser) throw new HttpException(409, `This email ${userData.email} was not found`);
-
-    const isPasswordMatching: boolean = await compare(userData.password, findUser.password);
-    if (!isPasswordMatching) throw new HttpException(409, 'Password not matching');
-
-    const tokenData = this.createToken(findUser);
-    const cookie = this.createCookie(tokenData);
-
-    return { cookie, findUser };
-  }
-
-  // 로그아웃
-  public async logout(userData: User): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
-
-    const findUser: User = await Users.findOne({ where: { email: userData.email, password: userData.password } });
-    if (!findUser) throw new HttpException(409, "User doesn't exist");
-
-    return findUser;
-  }
-
-  public createToken(user: User): TokenData {
-    const dataStoredInToken: DataStoredInToken = { id: user.id };
-    const secretKey: string = SECRET_KEY;
-    const expiresIn: number = 60 * 60;
-
-    return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
-  }
-
-  public createCookie(tokenData: TokenData): string {
-    return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn};`;
-  }
 }
 
 export default AuthService;
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -7,20 +7,6 @@ import { Follow } from '../entities/Follow';
 
 @EntityRepository()
 class UserService extends Repository<Users> {
-  public async findAllUser(): Promise<User[]> {
-    const users: User[] = await Users.find();
-    return users;
-  }
-
-  public async findUserById(userId: number): Promise<User> {
-    if (isEmpty(userId)) throw new HttpException(400, 'UserId is empty');
-
-    const findUser: User = await Users.findOne({ where: { id: userId } });
-    if (!findUser) throw new HttpException(409, "User doesn't exist");
-
-    return findUser;
-  }
-
   public async following(userId: number, followerId: number): Promise<void> {
     // followerId : (팔로우 당하는 사람 기준) 팔로우를 건 사람.
     // followingId : (팔로우 한사람 기준) 팔로우 건 상대 (현재 로그인한 유저)
@@ -34,16 +20,6 @@ class UserService extends Repository<Users> {
   public async updateUser(userId: number, newNick: string): Promise<void> {
     Users.update({ id: userId }, { nick: newNick });
   }
-
-  // public async deleteUser(userId: number): Promise<User> {
-  //   if (isEmpty(userId)) throw new HttpException(400, 'UserId is empty');
-
-  //   const findUser: User = await UserEntity.findOne({ where: { id: userId } });
-  //   if (!findUser) throw new HttpException(409, "User doesn't exist");
-
-  //   await UserEntity.delete({ id: userId });
-  //   return findUser;
-  // }
 }
 
 export default UserService;
