43bb89f08877132b9f58f14134f838955b99397a
qormsxo <45680973+qormsxo@users.noreply.github.com>
2023-01-04 08:18:42 +0900

    try/catch 리팩토링

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -10,19 +10,30 @@ class AuthController {
 
   public signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const userData: CreateUserDto = req.body;
-      const signUpUserData: User = await this.authService.signup(userData);
+      const signUpResult = await this.signUpByUserData(req.body);
 
-      res.status(201).json({ data: signUpUserData, message: 'signup' });
+      res.status(signUpResult.code).json(signUpResult);
     } catch (error) {
       next(error);
     }
   };
 
+  // try/catch 리팩토링
+  private signUpByUserData = async (userData: CreateUserDto) => {
+    const signUpUserData: User = await this.authService.signup(userData);
+    return {
+      code: 201,
+      data: signUpUserData,
+      message: 'signup',
+    };
+  };
+
+  // 회원가입 화면 렌더링
   public joinView(req: Request, res: Response) {
     res.render('join', { title: '회원가입 - NodeBird' });
   }
 
+  // 로그인
   public logIn(req: Request, res: Response, next: NextFunction) {
     passport.authenticate('local', (error: Error, user, info) => {
       if (error) {
@@ -43,6 +54,7 @@ class AuthController {
     })(req, res, next);
   }
 
+  // 로그아웃
   public logOut = async (req: RequestWithUser, res: Response, next: NextFunction) => {
     req.logOut(err => {
       if (err) return next(err);
